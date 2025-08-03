5fac9681edecaf3577108c0a784f8c12592290dd
ENZO F <enzoenzof2024@gmail.com>
2025-08-03 01:16:09 +0000

    pokracuj dalej

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -109,7 +109,7 @@ export function Header() {
     >
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
         <Link href="/" passHref>
-          <div className="text-xl font-bold font-headline text-primary cursor-pointer hover:animate-glow">
+          <div className="text-xl font-bold font-headline text-primary cursor-pointer hover:animate-text-glow">
             SOFTW4R3
           </div>
         </Link>
