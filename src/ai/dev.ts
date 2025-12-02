87014cd09fb6aad476e33fb16d7ca5bc9883584e
ENZO F <enzoenzof2024@gmail.com>
2025-12-02 22:24:07 +0000

    dokonci to

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -7,5 +7,4 @@ import '@/ai/flows/generate-plugin-marketing-copy.ts';
 import '@/ai/flows/generate-plugin-idea.ts';
 import '@/ai/flows/generate-audio-from-text.ts';
 import '@/ai/flows/generate-plugin-logo.ts';
-import '@/ai/flows/get-affiliate-stats.ts';
 
